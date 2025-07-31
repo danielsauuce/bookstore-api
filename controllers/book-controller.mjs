728c844be431909609008d39122d9129234e2740
Daniel <danieladeyd77@gmail.com>
2025-07-31 06:44:23 +0100

    book controllers updated

--- controllers/book-controller.mjs
+++ controllers/book-controller.mjs
@@ -4,7 +4,34 @@ import Book from '../models/book.mjs'
 const getAllBooks = async (req, res) => {
 };
 
-const getSingleBookById = async (req, res) => {};
+const getSingleBookById = async (req, res) => {
+    try {
+        const inputId = req.params.inputId
+        const getSingleBook = await Book.findById(inputId);
+
+        if(!getSingleBook) {
+            return res.status(404).json({
+                success: false,
+                message: "Unable to retrieve Book by that Id, try another valid Id",
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Book found",
+            data: getSingleBook
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong',
+            error: error.message,
+        })
+        
+    }
+};
 
 const addNewBook = async (req, res) => {
     try {
@@ -20,6 +47,7 @@ const addNewBook = async (req, res) => {
         }
 
     } catch (error) {
+        console.log(error)
         res.status(500).json({
             success: false,
             message: 'Something went wrong',
@@ -29,7 +57,36 @@ const addNewBook = async (req, res) => {
     }
 };
 
-const updateBook = async (req, res) => {};
+const updateBook = async (req, res) => {
+    try {
+        const getFormData = req.body;
+        const inputId = req.params.id;
+        
+        const updateBook = await Book.findByIdAndUpdate(inputId, getFormData, {new: true});
+        
+        if(!updateBook) {
+            return res.status(404).json({
+                success: false,
+                message: "Unable to update Book, Invalid id."
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Book Updated Successfully",
+            data: updateBook
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong',
+            error: error.message,
+        })
+        
+    }
+};
 
 const deleteBook = async (req, res) => {};
 
